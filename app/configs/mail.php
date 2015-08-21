2047bcb92994968b18803c9c2eb5548fe378ff2a
anonymframework <serif.saglam119@gmail.com>
2015-08-21 20:16:32 +0300

    added comment lines

---
+++ app/configs/mail.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+return [
+
+    'driver' => 'swift', // phpmailer or swift
+    /**
+     * |*********************************
+     * |
+     * | Anonym Framework Mail configs;
+     * | ---------------------------
+     * | variables;
+     * |
+     * | username => the username of mail
+     * | password => the password of mail
+     * | port =>     the port of mail
+     * | secure =>   the secure of mail,use it  only in phpmailer driver!
+     * |
+     * |*********************************
+     */
+
+
+
+];
