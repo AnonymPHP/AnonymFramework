bd2c3e639b52f08b1e9895a28911714cd6302238
anonymframework <serif.saglam119@gmail.com>
2015-08-21 09:44:18 +0300

    added some constants

---
+++ system/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+/**
+ *
+ *  the constant for application dir
+ *
+ */
+define('APP', 'app/');
+
+/**
+ *
+ * the constant for config dir
+ *
+ */
+
+define('CONFIG', APP . 'configs/');
