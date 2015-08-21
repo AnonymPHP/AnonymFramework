d8116c2530f07719d11a2d260d1ab8800ca13b9c
anonymframework <serif.saglam119@gmail.com>
2015-08-22 02:11:09 +0300

    added test controller

---
+++ app/http/controllers/Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Controllers;
+
+
+use Anonym\Components\Route\Controller;
+
+class Test extends Controller
+{
+
+}
