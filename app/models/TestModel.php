6c6ae60045eb7999f7141370c2bc69e3b0dafbd1
anonymframework <serif.saglam119@gmail.com>
2015-08-22 10:34:08 +0300

    added test model

---
+++ app/models/TestModel.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Models;
+
+
+use Anonym\Components\Facade\Element;
+
+/**
+ * Class TestModel
+ * @package Anonym\Models
+ */
+class TestModel extends Element
+{
+
+}
