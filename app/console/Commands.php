b4422e9209b8bba146f0cca41446ac269d6db0cf
anonymframework <serif.saglam119@gmail.com>
2015-08-22 09:12:36 +0300

    fiixed a little bug

---
+++ app/console/Commands.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Console;
+
+
+class Commands
+{
+
+}
