276e6cfa56f2bde54f013d72f4ff453ad78c6e0d
anonymframework <serif.saglam119@gmail.com>
2015-08-22 10:36:59 +0300

    made some updates

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -9,40 +9,41 @@
  */
 
 use Anonym\Support\Arr;
-/**
- * get the callable value
- *
- * @param callable $callback
- * @return mixed
- */
-function value($callback = null)
-{
-    if(is_callable($callback))
+
+
+if (!function_exists('value')) {
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed $value
+     * @return mixed
+     */
+    function value($value)
     {
-        return call_user_func($callback);
-    }elseif(is_array($callback)){
-        return array_values($callback)[0];
-    }else{
-        return $callback;
+        return $value instanceof Closure ? $value() : $value;
     }
 }
 
-/**
- * @param array $target
- * @param string|array $keys
- * @param callable|null $default
- */
-function data_get($target = [], $keys = '', $default = null)
-{
-    if(is_object($target))
+if (!function_exists('data_get')) {
+    /**
+     * return the value
+     *
+     * @param array $target
+     * @param string|array $keys
+     * @param callable|null $default
+     */
+    function data_get($target = [], $keys = '', $default = null)
     {
-        $target = (array) $target;
+        if (is_object($target)) {
+            $target = (array)$target;
+        }
+
+        if (is_array($keys)) {
+            $keys = join(',', $keys);
+        }
+
+        Arr::get($target, $keys, $default);
     }
 
-    if(is_array($keys))
-    {
-        $keys = join(',', $keys);
-    }
-
-    Arr::get($target, $keys, $default);
 }
+
