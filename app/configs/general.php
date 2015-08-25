062222b6ff8d4922a040adeb18c722129d2dda25
anonymframework <serif.saglam119@gmail.com>
2015-08-25 12:33:21 +0300

    removed database provider

--- app/configs/general.php
+++ app/configs/general.php
@@ -38,7 +38,6 @@ return [
      */
     'providers' => [
         \Anonym\Providers\EventProvider::class,
-        \Anonym\Providers\DatabaseProvider::class,
         \Anonym\Providers\RouteProvider::class
     ],
 
