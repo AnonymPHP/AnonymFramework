4daa33f140b38013948794be4876e4f5e1081b4c
anonymframework <serif.saglam119@gmail.com>
2015-08-25 06:55:49 +0300

    added csrf

---
+++ app/configs/security.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+return [
+
+    'csrf' => [
+        'status' => true, // make it false, if you dont want use it
+        'field_name'=> '_token' // this string will be look in posts
+    ]
+
+];
