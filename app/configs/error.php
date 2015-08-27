5e77e6d95dfbc982aed09e7a695fbdb9668c77d9
anonymframework <serif.saglam119@gmail.com>
2015-08-27 08:43:10 +0300

    added error configs

--- app/configs/error.php
+++ app/configs/error.php
@@ -8,3 +8,34 @@
  * Thanks for using
  */
 
+return [
+
+    /**
+     * | ****************
+     * |
+     * | these configs for handle the error or exceptions
+     * |
+     * | *****************
+     */
+
+    'handle' => [
+
+        /**
+         *
+         *  true for enable, false to disable
+         *
+         */
+
+        'errors'     => [
+            'switch'  => E_USER_ERROR,
+            'enabled' => true
+        ],
+        /**
+         *
+         *  true for enable, false to disable
+         *
+         */
+        'exceptions' => true,
+    ]
+
+];
