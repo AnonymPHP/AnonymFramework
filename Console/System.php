c6513fa9ce7cbdbbbb5e5f91c87eab340925f2f9
anonymframework <serif.saglam119@gmail.com>
2015-08-27 10:03:24 +0300

    made some updates

---
+++ Console/System.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Console;
+
+/**
+ * Class System
+ * @package Console
+ */
+class System
+{
+
+    /**
+     * the repository of console commands
+     *
+     * @var array
+     */
+    protected $commands =
+        [
+
+        ];
+
+}
