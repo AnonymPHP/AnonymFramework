ff2e8d8d02b80878a50db523c667ffe8db76765b
anonymframework <serif.saglam119@outlook.com>
2015-09-01 17:03:06 +0300

    added register method

--- App/Services/RegisterService.php
+++ App/Services/RegisterService.php
@@ -17,4 +17,13 @@ use Anonym\Bootstrap\ServiceProvider;
 class RegisterService extends ServiceProvider
 {
 
+    /**
+     * register the provider
+     *
+     * @return mixed
+     */
+    public function register()
+    {
+
+    }
 }
