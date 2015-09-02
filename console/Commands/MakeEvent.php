d42d320ff83fa4339274602da96a2d7f5e3ebfe2
anonymframework <serif.saglam119@outlook.com>
2015-09-02 10:32:05 +0300

    added make event

---
+++ console/Commands/MakeEvent.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+
+namespace Console\Commands;
+
+
+use Anonym\Components\Console\Command;
+
+/**
+ * Class MakeEvent
+ * @package Console\Commands
+ */
+class MakeEvent extends Command
+{
+
+}
