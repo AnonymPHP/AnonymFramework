9dc71f961e0220964e813571569f081f93561f25
anonymframework <serif.saglam119@outlook.com>
2015-09-02 10:35:00 +0300

    added handle interfac

--- console/Commands/MakeEvent.php
+++ console/Commands/MakeEvent.php
@@ -14,6 +14,8 @@ namespace Console\Commands;
 
 use Anonym\Components\Console\Command;
 use Anonym\Components\Console\HandleInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
 
 /**
  * Class MakeEvent
@@ -45,4 +47,14 @@ class MakeEvent extends Command implements HandleInterface
         parent::__construct();
     }
 
+    /**
+     * Komut yakalandığı zaman tetiklenecek fonksiyonlardan biridir
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    public function handle(InputInterface $input, OutputInterface $output)
+    {
+
+    }
 }
