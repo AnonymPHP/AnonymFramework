a9be16b16d9d9b51c2ce7865c47dbf9abbb9e5a8
anonymframework <serif.saglam119@outlook.com>
2015-09-04 11:12:09 +0300

    added make controller method to console

---
+++ console/Commands/MakeController.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+
+namespace Console\Commands;
+
+
+use Anonym\Components\Console\Command;
+use Anonym\Components\Console\HandleInterface;
+use Anonym\Facades\Stroge;
+use Anonym\Support\TemplateGenerator;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+
+/**
+ * Class MakeController
+ * @package Console\Commands
+ */
+class MakeController extends Command implements HandleInterface
+{
+
+    /**
+     * the signature of command
+     *
+     * @var string
+     */
+    protected $signature = 'make:controller {name}';
+
+
+    /**
+     * the description of command
+     *
+     * @var string
+     */
+    protected $description = 'create an controller';
+
+    /**
+     * create an event instance
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Komut yakalandığı zaman tetiklenecek fonksiyonlardan biridir
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    public function handle(InputInterface $input, OutputInterface $output)
+    {
+        $content = file_get_contents(RESOURCE.'migrations/event.php.dist');
+
+        $generator = new TemplateGenerator($content);
+        $name = $this->argument('name');
+
+        $path = 'Events/'.$name.'.php';
+        $generated = $generator->generate(['name' => $name]);
+
+        if (Stroge::exists($path)) {
+            Stroge::create($path);
+            Stroge::put($path, $generated);
+            $this->info(sprintf('%s created succesfully to %s', $name, $path));
+        } else {
+            $this->error(sprintf('%s Event already exists', $name));
+        }
+    }
+}
