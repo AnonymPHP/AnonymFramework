d12105f3ec3d2bae218a0353a49d6279bde4d136
anonymframework <serif.saglam119@outlook.com>
2015-09-04 22:25:12 +0300

    added view serive

---
+++ App/Services/ViewService.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+
+namespace App\Services;
+
+
+use Anonym\Bootstrap\ServiceProvider;
+
+/**
+ * Class ViewService
+ * @package App\Services
+ */
+class ViewService extends ServiceProvider
+{
+
+}
