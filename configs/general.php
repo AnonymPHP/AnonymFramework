5355db8015f4acace38d5c12235a30d8d16834f5
anonymframework <serif.saglam119@outlook.com>
2015-09-05 22:19:27 +0300

    made some updates

--- configs/general.php
+++ configs/general.php
@@ -29,6 +29,7 @@ return [
         'event' => \Anonym\Components\Event\EventDispatcher::class,
         'route' => \Anonym\Components\Route\RouteCollector::class,
         'app' => \Anonym\Container\Container::class,
+        'guard' => \Anonym\Components\Security\Authentication\Guard::class,
         'redirect' => \Anonym\Components\HttpClient\Redirect::class,
         'config' => \Anonym\Components\Config\Reposity::class,
     ],
