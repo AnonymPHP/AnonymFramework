5386533201e1c39df881c3493e0d5a6dc73e5691
anonymframework <serif.saglam119@outlook.com>
2015-09-06 11:13:40 +0300

    added handle method

--- console/Commands/MakeModel.php
+++ console/Commands/MakeModel.php
@@ -14,8 +14,20 @@ namespace Console\Commands;
 
 use Anonym\Components\Console\Command;
 use Anonym\Components\Console\HandleInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
 
 class MakeModel extends Command implements HandleInterface
 {
 
+    /**
+     * Komut yakalandığı zaman tetiklenecek fonksiyonlardan biridir
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    public function handle(InputInterface $input, OutputInterface $output)
+    {
+        // TODO: Implement handle() method.
+    }
 }
