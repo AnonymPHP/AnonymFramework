acacfe09ba9f8365690133bb04526a2dce1dfa93
anonymframework <serif.saglam119@outlook.com>
2015-09-13 02:10:47 +0300

    made some updates

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AnonymPHP</title>
+
+        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+
+            .title-micro{
+                color: #787171;
+                font-size: 25px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Anonym 2</div>
+
+                <p class="title-micro">Professional PHP Framework</p>
+            </div>
+        </div>
+    </body>
+</html>
