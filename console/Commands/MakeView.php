08d31e24653ef74f04e7218aca5642a80c1c1f38
anonymframework <serif.saglam119@outlook.com>
2015-09-13 20:14:58 +0300

    made some update

--- console/Commands/MakeView.php
+++ console/Commands/MakeView.php
@@ -13,10 +13,32 @@ namespace Console\Commands;
 
 use Anonym\Components\Console\Command;
 use Anonym\Components\Console\HandleInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
 
 class MakeView extends Command implements HandleInterface
 {
 
 
+    /**
+     *
+    /**
+     * @var string
+     */
+    protected $signature = 'make:view {name}';
 
+    /**
+     * @var string
+     */
+    protected $description = "create a new model file";
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    public function handle(InputInterface $input, OutputInterface $output)
+    {
+        // TODO: Implement handle() method.
+    }
 }
