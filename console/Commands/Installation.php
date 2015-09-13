cae7744b33e6decac082f849c88e14546fcf6f45
anonymframework <serif.saglam119@outlook.com>
2015-09-14 02:05:17 +0300

    made some updates

--- console/Commands/Installation.php
+++ console/Commands/Installation.php
@@ -13,8 +13,20 @@ namespace Console\Commands;
 
 use Anonym\Components\Console\Command;
 use Anonym\Components\Console\HandleInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
 
 class Installation extends Command implements HandleInterface
 {
 
+    /**
+     * Komut yakalandığı zaman tetiklenecek fonksiyonlardan biridir
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    public function handle(InputInterface $input, OutputInterface $output)
+    {
+        // TODO: Implement handle() method.
+    }
 }
