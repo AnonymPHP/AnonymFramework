81884e020ec9a50192be03c245b1f3a1f3ad334d
anonymframework <serif.saglam119@outlook.com>
2015-09-14 02:10:17 +0300

    made some updates

--- console/Commands/CleanView.php
+++ console/Commands/CleanView.php
@@ -13,6 +13,7 @@ namespace Console\Commands;
 
 use Anonym\Components\Console\Command;
 use Anonym\Components\Console\HandleInterface;
+use Anonym\Components\Filesystem\Filesystem;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 
@@ -20,11 +21,24 @@ class CleanView extends Command implements HandleInterface
 {
 
     /**
+     * the description of command
+     *
+     * @var string
+     */
+    protected $description = 'clean the all view files';
+
+    protected $name = 'clean:view';
+
+    public function __construct(Filesystem $filesystem){
+
+    }
+    /**
      * @param InputInterface $input
      * @param OutputInterface $output
      * @return mixed
      */
     public function handle(InputInterface $input, OutputInterface $output)
     {
+
     }
 }
