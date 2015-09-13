f555e0437c11a5e91c2e8694cfebfdbf068081fa
anonymframework <serif.saglam119@outlook.com>
2015-09-14 02:09:18 +0300

    made some updates

--- console/Commands/CleanView.php
+++ console/Commands/CleanView.php
@@ -13,8 +13,18 @@ namespace Console\Commands;
 
 use Anonym\Components\Console\Command;
 use Anonym\Components\Console\HandleInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
 
 class CleanView extends Command implements HandleInterface
 {
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    public function handle(InputInterface $input, OutputInterface $output)
+    {
+    }
 }
