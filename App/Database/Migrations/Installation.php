6710b40ee655e7640c4fb625ead3c039adf4b6de
anonymframework <serif.saglam119@outlook.com>
2015-09-14 02:00:17 +0300

    made some updates

---
+++ App/Database/Migrations/Installation.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @copyright GemMedya, 2015
+ */
+
+namespace App\Database\Migrations;
+
+use Anonym\Components\Tools\Migration;
+use Anonym\Components\Tools\MigrationInterface;
+use Anonym\Components\Tools\Table;
+use Anonym\Facades\Schema;
+
+class Installation extends Migration implements MigrationInterface
+{
+
+    /**
+     * Yükleme İşlemleri burada gerçekleşir
+     */
+
+    public function up()
+    {
+
+        Schema::create('sessions', function (Table $table) {
+            return $table->varchar('key')
+                ->text('value');
+        });
+
+
+    }
+
+
+    /**
+     * Düşürme işlemleri burada gerçekleşir
+     */
+    public function down()
+    {
+        //
+    }
+}
