638db89ab1adc19e5ef0d2ec4f7a56b4aa8704f1
anonymframework <serif.saglam119@outlook.com>
2015-09-13 20:29:36 +0300

    made some update

--- console/Commands/MakeView.php
+++ console/Commands/MakeView.php
@@ -13,6 +13,7 @@ namespace Console\Commands;
 
 use Anonym\Components\Console\Command;
 use Anonym\Components\Console\HandleInterface;
+use Anonym\Filesystem\Filesystem;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 
@@ -33,12 +34,30 @@ class MakeView extends Command implements HandleInterface
     protected $description = "create a new model file";
 
     /**
+     * the instance of filesystem
+     *
+     * @var Filesystem
+     */
+    protected $filesystem;
+
+    public function __construct(Filesystem $filesystem){
+        $this->filesystem = $filesystem;
+        parent::__construct();
+    }
+    /**
      * @param InputInterface $input
      * @param OutputInterface $output
      * @return mixed
      */
     public function handle(InputInterface $input, OutputInterface $output)
     {
+        $name = $this->argument('name');
+        $path = VIEW.$name.'.php';
 
+        if (!$this->filesystem->exists($path)) {
+            $this->filesystem->create($path);
+        }
+
+        $this->info(sprintf('%s wiew file is created with succesfully', $name));
     }
 }
