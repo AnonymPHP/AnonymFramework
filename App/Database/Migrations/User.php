010efc7ad710c077f9e48505e31e0876b07aab6b
anonymframework <serif.saglam119@outlook.com>
2015-09-14 12:52:12 +0300

    made some updates

---
+++ App/Database/Migrations/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @copyright GemMedya, 2015
+ */
+
+namespace App\Database\Migrations;
+
+use Anonym\Components\Tools\Migration;
+use Anonym\Components\Tools\MigrationInterface;
+
+class User extends Migration implements MigrationInterface
+{
+
+    /**
+     * Yükleme İşlemleri burada gerçekleşir
+     */
+
+    public function up()
+    {
+
+    }
+
+
+    /**
+     * Düşürme işlemleri burada gerçekleşir
+     */
+    public function down()
+    {
+        //
+    }
+}
