37e9e5cf07793aabcd8865d2d94f7641d19558c3
anonymframework <serif.saglam119@outlook.com>
2015-09-14 14:48:02 +0300

    made some updates

---
+++ console/Commands/MakeSeedCommand.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Console\Commands;
+
+
+use Anonym\Components\Console\Command;
+use Anonym\Components\Console\HandleInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+
+class MakeSeedCommand extends Command implements HandleInterface
+{
+
+    /**
+     *
+    /**
+     * @var string
+     */
+    protected $signature = 'make:seed {name}';
+
+    /**
+     * @var string
+     */
+    protected $description = "create a seed file";
+
+
+    /**
+     * Komut yakalandığı zaman tetiklenecek fonksiyonlardan biridir
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    public function handle(InputInterface $input, OutputInterface $output)
+    {
+        // TODO: Implement handle() method.
+    }
+}
