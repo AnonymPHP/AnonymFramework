de6ee60612922f42b3a4cdd3f9862a8efd155cc9
anonymframework <serif.saglam119@outlook.com>
2015-09-14 16:40:47 +0300

    made some updates

---
+++ console/Commands/DeploySeedAllCommand.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Console\Commands;
+
+
+use Anonym\Components\Console\Command;
+use Anonym\Components\Console\HandleInterface;
+use Anonym\Components\Tools\Seeder;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * Class DeploySeedCommand
+ * @package Console\Commands
+ */
+class DeploySeedAllCommand extends Command implements HandleInterface
+{
+
+    /**
+     * the name of signature
+     *
+     * @var string
+     */
+    protected $signature = 'seed:all';
+
+    /**
+     * @var string
+     */
+    protected $description = "run all seed files";
+
+    /**
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    public function handle(InputInterface $input, OutputInterface $output)
+    {
+
+        $seeder = new Seeder($this->getContainer());
+        $seeder->setCommand($this);
+
+        foreach($this->readAllSeeds() as $name){
+            $seeder->call($name);
+        }
+    }
+
+    /**
+     *
+     * read all seeds name
+     */
+    public function readAllSeeds()
+    {
+        $list = Finder::create()->files()->name('*.php')->in(MIGRATION);
+
+        $result = [];
+        foreach ($list as $l) {
+            $exp = first(explode('.', $l->getFilename()));
+            $result[] = end(explode('/', $exp));
+        }
+
+        return $result;
+    }
+}
