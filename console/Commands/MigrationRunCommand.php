738bff2110ad411accddb22aefe90b788f9f1e27
anonymframework <serif.saglam119@outlook.com>
2015-09-14 14:19:00 +0300

    made some updates

---
+++ console/Commands/MigrationRunCommand.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Console\Commands;
+use Anonym\Components\Console\HandleInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Anonym\Components\Console\Command;
+use Anonym\Facades\Anonym;
+/**
+ * Class MakeMigrationCommand
+ * @package Console\Commands
+ */
+class MakeMigrationRun extends Command implements HandleInterface
+{
+    /**
+     *
+     * /**
+     * @var string
+     */
+    protected $signature = 'migration:run {name?}';
+
+    /**
+     * @var string
+     */
+    protected $description = "run a migration file, or more.";
+
+
+
+    /**
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    public function handle(InputInterface $input, OutputInterface $output)
+    {
+        $name = $this->argument('name') ? $this->argument('name') : '';
+
+        Anonym::call('migration', [
+            'function' => 'deploy', 'name' => $name
+        ]);
+
+        $this->info(sprintf('%s migration file created with successfully', $name));
+    }
+}
