2d51116307fc55114f86875defd42c31e6c287e1
anonymframework <serif.saglam119@outlook.com>
2015-09-14 14:54:13 +0300

    made some updates

--- console/Commands/MakeSeedCommand.php
+++ console/Commands/MakeSeedCommand.php
@@ -13,6 +13,8 @@ namespace Console\Commands;
 
 use Anonym\Components\Console\Command;
 use Anonym\Components\Console\HandleInterface;
+use Anonym\Filesystem\Filesystem;
+use Anonym\Support\TemplateGenerator;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 
@@ -33,6 +35,18 @@ class MakeSeedCommand extends Command implements HandleInterface
 
 
     /**
+     * @var Filesystem
+     */
+    protected $file;
+
+    /**
+     * @param Filesystem $filesystem
+     */
+    public function __construct(Filesystem $filesystem){
+        $this->file = $filesystem;
+        parent::__construct();
+    }
+    /**
      *
      * @param InputInterface $input
      * @param OutputInterface $output
@@ -40,6 +54,21 @@ class MakeSeedCommand extends Command implements HandleInterface
      */
     public function handle(InputInterface $input, OutputInterface $output)
     {
+        $template = new TemplateGenerator($this->file->get(RESOURCE.'migrations/seed.php.dist'));
 
+        $name = $this->argument('name');
+
+        $content = $template->generate([
+            'name' => $name
+        ]);
+
+        $path = "App/Database/Seeds/$name.php";
+
+        if (!$this->file->exists($path)) {
+            $this->file->create($path);
+            $this->file->put($path, $content);
+        }
+
+        $this->info(sprintf('%s seed created successfully', $name));
     }
 }
