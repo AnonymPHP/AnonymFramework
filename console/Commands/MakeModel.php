27c51e5ef61243b3073c86ad2aebd77a777e005d
anonymframework <serif.saglam119@outlook.com>
2015-09-17 02:14:30 +0300

    made some updates

--- console/Commands/MakeModel.php
+++ console/Commands/MakeModel.php
@@ -14,6 +14,7 @@ namespace Console\Commands;
 
 use Anonym\Components\Console\Command;
 use Anonym\Components\Console\HandleInterface;
+use Anonym\Filesystem\Filesystem;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Anonym\Support\TemplateGenerator;
@@ -41,8 +42,26 @@ class MakeModel extends Command implements HandleInterface
      */
     protected $description = "create a new model file";
 
+
     /**
-     * Komut yakalandığı zaman tetiklenecek fonksiyonlardan biridir
+     * the instance of filesystem
+     *
+     * @var Filesystem
+     */
+    private $file;
+
+    /**
+     * create a new instance and register filesystem variable
+     *
+     * @param Filesystem $filesystem
+     */
+    public function __construct(Filesystem $filesystem){
+        $this->file = $filesystem;
+
+        parent::__construct();
+    }
+    /**
+     *
      * @param InputInterface $input
      * @param OutputInterface $output
      * @return mixed
@@ -50,19 +69,23 @@ class MakeModel extends Command implements HandleInterface
     public function handle(InputInterface $input, OutputInterface $output)
     {
 
-        $content = file_get_contents(RESOURCE.'migrations/model.php.dist');
+        // now we will read model stub file
+        $content = $this->file->get(RESOURCE.'migrations/model.php.dist');
 
         $generator = new TemplateGenerator($content);
         $name = $this->argument('name');
 
         $path = 'App/Models/'.$name.'.php';
+
+        // create content
         $generated = $generator->generate(['name' => $name]);
-        if (!Stroge::exists($path)) {
-            Stroge::create($path);
-            Stroge::put($path, $generated);
-            $this->info(sprintf('%s created succesfully to %s', $name, $path));
-        } else {
-            $this->error(sprintf('%s Model already exists', $name));
+        if (!$this->file->exists($path)) {
+            $this->file->create($path);
+
+            // write the created content to model file
+            $this->file->put($path, $generated);
         }
+        $this->info(sprintf('%s created succesfully to %s', $name, $path));
+
     }
 }
