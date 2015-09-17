a7d626af64a3b60f835d67d0d4bc859a16da9b4a
anonymframework <serif.saglam119@outlook.com>
2015-09-17 22:45:09 +0300

    made some updates

--- console/Commands/MakeBackupCommand.php
+++ console/Commands/MakeBackupCommand.php
@@ -9,10 +9,11 @@
  */
 
 namespace Console\Commands;
-
-
 use Anonym\Components\Console\Command;
+use Anonym\Facades\Backup as BackupFacade;
 use Anonym\Components\Console\HandleInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class MakeBackupCommand extends Command implements HandleInterface
 {
@@ -22,7 +23,7 @@ class MakeBackupCommand extends Command implements HandleInterface
      *
      * @var string
      */
-    protected $signature = 'make:backup {tables?}';
+    protected $signature = 'make:backup {name} {tables?}';
 
     /**
      * the description of command
@@ -31,4 +32,15 @@ class MakeBackupCommand extends Command implements HandleInterface
      */
     protected $description = 'create a new database backup file';
 
+    public function handle(InputInterface $input, OutputInterface $output){
+        $name = $this->argument('name');
+        $tables = $this->argument('tables') ? $this->argument('tables') : '*';
+        $return = BackupFacade::backup($tables, $name);
+        if (true === $return) {
+            $this->info(sprintf('%s backup file generated in %s path', $name,
+                BackupLoader::generatePath($name)));
+        } else {
+            $this->error(sprintf('%s cant create, file already exists', $name));
+        }
+    }
 }
