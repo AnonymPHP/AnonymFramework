f64015077b7014ad9815b2b04f978388fe723e0b
anonymframework <serif.saglam119@outlook.com>
2015-09-24 07:46:08 +0300

    made some updates

---
+++ console/Commands/MakeCommand.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Console\Commands;
+
+
+use Anonym\Components\Console\Command;
+use Anonym\Components\Console\HandleInterface;
+use Anonym\Filesystem\Filesystem;
+use Anonym\Support\TemplateGenerator;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+
+/**
+ * Class MakeCommand
+ * @package Console\Commands
+ */
+class MakeCommand extends Command implements HandleInterface
+{
+
+    /**
+     * the signature of command
+     *
+     * @var string
+     */
+    protected $signature = 'make:command {name}';
+
+    /**
+     * the description of command
+     *
+     * @var string
+     */
+    protected $description = '';
+
+
+    /**
+     * the instance of filesystem
+     *
+     * @var Filesystem
+     */
+    protected $file;
+
+    /**
+     * create a new isntance and register filesystem
+     *
+     * @param Filesystem $filesystem
+     */
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->file = $filesystem;
+    }
+
+    /**
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    public function handle(InputInterface $input, OutputInterface $output)
+    {
+        $content = $this->file->get(RESOURCE . 'migrations/command.php.dist');
+
+        $name = $this->argument('name');
+
+        $template = new TemplateGenerator($content);
+        $generated = $template->generate([
+            'name' => $name
+        ]);
+
+
+        $path = BASE . 'console/Commands/' . $name . '.php';
+        $this->file->put($path, $generated);
+
+        $this->info(sprintf('%s command created with successfully in %s', $name, $path));
+    }
+}
