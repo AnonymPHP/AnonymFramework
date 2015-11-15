f5c75422e7e426edc7dbde17243912e385569c39
anonymframework <anonymframework@gemframework.com>
2015-11-15 14:44:32 +0200

    made some updates

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -17,6 +17,7 @@ use Anonym\Facades\App;
 use Anonym\Facades\Cookie;
 use Anonym\Facades\Event;
 use Anonym\Facades\Csrf;
+use Anonym\Support\Str;
 use Illuminate\Contracts\Support\Htmlable;
 
 
@@ -799,3 +800,45 @@ if (!function_exists('group')) {
     }
 }
 
+/**
+ * | *******************
+ * |
+ * | determine is there any function with same name
+ * |
+ * |*********************
+ */
+if (! function_exists('env')) {
+
+    /**
+     * Gets the value of an environment variable. Supports boolean, empty and null.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+        if ($value === false) {
+            return value($default);
+        }
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+        if (Str::startsWith($value, '"') && Str::endsWith($value, '"')) {
+            return substr($value, 1, -1);
+        }
+        return $value;
+    }
+}
