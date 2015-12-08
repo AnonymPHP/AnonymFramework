214ce5c849cc238a7213a210121dc5cb665c0168
anonymframework <anonymframework@gemframework.com>
2015-12-08 14:14:55 +0200

    made some updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,55 +1,47 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>AnonymPHP</title>
+<head>
+    <title>AnonymPHP</title>
 
-        <link rel="stylesheet" href="{{ asset('style.css') }}">
-        <link rel="stylesheet" href="{{ asset('bootstrap.css') }}">
-        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="stylesheet" href="{{ asset('bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+</head>
+<body data-spy="scroll" data-target=".navMenuCollapse">
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+<div id="wrap">
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-
-            .title-micro{
-                color: #787171;
-                font-size: 25px;
-            }
-        </style>
-    </head>
-    <body>
+    <!-- NAVIGATION -->
+    <!-- SUBSCRIBE HALF LEFT BLOCK --><!-- FOOTER -->
+    <!-- NAVIGATION -->
+    <nav class="navbar bg-color3">
         <div class="container">
-            <div class="content">
-                <div class="title">Anonym 2</div>
-
-                <p class="title-micro">Professional PHP Framework</p>
-                <p class="title-micro">IT is WORKING!</p>
+            <a class="navbar-brand goto" href="#"><img height="32" alt="Your logo" src="{{ asset('logo.png') }}"></a>
+            <button class="round-toggle navbar-toggle menu-collapse-btn collapsed" data-toggle="collapse"
+                    data-target=".navMenuCollapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span
+                        class="icon-bar"></span></button>
+            <div class="collapse navbar-collapse navMenuCollapse">
+                <ul class="nav">
+                    <li><a href="kophack.com">Documantation</a></li>
+                </ul>
             </div>
         </div>
-    </body>
+    </nav><!-- TEXT 1COL BLOCK -->
+    <section id="text-1col">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <center>
+                        <h2>AnonymPHP Framework</h2>.
+                        <h5 class="sub-title">The best PHP Framework</h5>
+                    </center>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+</body>
 </html>
