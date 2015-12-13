bd5bc23f3f9aaf69e97f94d78696b4361feb810f
anonymframework <anonymframework@gemframework.com>
2015-12-13 14:09:13 +0200

    made some updates

---
+++ configs/billing.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+return [
+
+    /**
+     * | ****************************************
+     * |
+     * | the table name of billing in database
+     * |
+     * | *****************************************
+     */
+    'table_name' => 'billing',
+
+    /**
+     *  the plans and their days.
+     *
+     */
+    'plans' => [
+        'weekly' => 7,
+        'monthly' => 30,
+        'yearly' => 365,
+    ]
+];
